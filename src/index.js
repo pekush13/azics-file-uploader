3ad24198df812efd2facfc43cd5b469ea4c6642a
Alexey Zamasskin <zamasskin@yandex.ru>
2021-05-03 19:03:30 +0300

    Верстка экрана

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './scss/main.scss';
 
+import imageUpload from './images/upload.svg';
+
 const className = 'azics-file-uploader';
 
-const upload = () => <div className={className}>1234</div>;
+const upload = () => (
+  <div className={className}>
+    <div className="afu__wrap">
+      <div className="afu__work-area">
+        <i className="afu__icon"></i>
+        <div className="afu__text">
+          <p>Drag&Drop files here</p>
+          <p>or</p>
+        </div>
+        <button className="afu__button">Browse Files</button>
+      </div>
+    </div>
+  </div>
+);
 
 console.log(upload());
 
